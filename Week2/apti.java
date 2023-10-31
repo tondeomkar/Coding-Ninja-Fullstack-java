65ee9a959b0ad9fd621d89e2dc867a23338b3b27
Omkar tonde <111636424+tondeomkar@users.noreply.github.com>
2023-10-31 16:48:21 +0530

    Add files via upload

---
+++ Week2/apti.java
@@ -0,0 +1,11 @@
+public class apti{
+    public static void main (String [] arg){
+        int i=10;
+    while((i=i-1)>0)
+    {
+        System.out.print(i);
+        if(i%5==0)
+            return;
+    }
+    }
+}
