e04d3a50e9d6bcd07d09b4147fc9b7f31b9cc4f4
Omkar tonde <111636424+tondeomkar@users.noreply.github.com>
2023-10-31 16:23:23 +0530

    Add files via upload

---
+++ Week2/SumOfEvenNo.java
@@ -0,0 +1,33 @@
+/*
+	
+	Pre-requisite to solve this problem:
+	In this question, we have to find sum of all even numbers till n. To take sum of all the even numbers, we have to take a variable and initialize it to zero. Let us name this variable as sum. 
+	Following code is used to add 2 to sum variable and update it to the same sum variable:
+	sum = sum + 2 
+	
+	Hint to solve this problem: We will have to loop on even numbers only and add the even numbers to the same sum variable, described above.
+*/  
+
+import java.util.*;
+public class SumOfEvenNo {
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 2; i <= N; i += 2) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    
+
+	}
+
+}
