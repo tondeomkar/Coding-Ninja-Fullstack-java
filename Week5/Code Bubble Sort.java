f7d410f4efddf4c0fbb36a57ce3e742064d86dc5
Omkar tonde <111636424+tondeomkar@users.noreply.github.com>
2023-12-03 18:48:15 +0530

    Code Bubble Sort

---
+++ Week5/Code Bubble Sort.java
@@ -0,0 +1,18 @@
+public class  {  
+
+    public static void bubbleSort(int[] arr){
+    	//Your code goes here
+        for (int i=0;i<arr.length;i++)
+        {
+            for (int j=0;j<arr.length-i-1;j++)
+            {
+                if (arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+            		arr[j]=arr[j+1];
+            		arr[j+1]=temp;
+                }
+            }
+        }
+    }  
+}
