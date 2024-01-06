9bc90934b57d1997718c60ad780af42072d1cc90
Omkar tonde <111636424+tondeomkar@users.noreply.github.com>
2024-01-06 22:12:30 +0530

    Polymorphism

--- OOP'S/Vehicle.java
+++ OOP'S/Vehicle.java
@@ -1,7 +1,21 @@
+package vehicle;
 
 public class Vehicle {
-	String color;
-	int maxSpeed;
+	protected String color;
+	private int maxSpeed;
+	
+	public Vehicle(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+		System.out.println("Vehicle Constructor");
+	}
+	
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
 	
 	public void print() {
 		System.out.println("Vehicle color : " + color);
